f121a06b768b8923432c7f27ba220c9ce24a5acf
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2025-02-13 20:05:45 +0600

    init

--- android/app/src/main/kotlin/com/example/flutterfly/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterfly/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.flutterfly
-
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity()
+
+AppCenter.start(getApplication(), "fb3bbf66-01c1-4041-9680-14eb1a758523",
+Analytics.class, Crashes.class);
